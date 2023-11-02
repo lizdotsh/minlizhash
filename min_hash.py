1fca464810a407ebc5a850d29ac2eea258956045
liz <lizjunkbox@gmail.com>
2023-11-01 22:04:46 -0700

    vectorized

--- min_hash.py
+++ min_hash.py
@@ -1,37 +1,29 @@
 import numpy as np
+import numpy.typing as npt
 import farmhash
 from dataclasses import dataclass
 from typing import Callable, List
 from functools import partial
+#from nptyping import NDArray, Structure, Shape, String
 
 
-# @dataclass
-# class HashElement:
-#     hash_gen: Callable[[str], np.uint64]
-#     salt: str
-#     def calc_hash(self, string: str) -> np.uint64:
-#         return self.hash_gen(string + self.salt)
-#salt_len: int
-
-
-
-HashElm = Callable[[str], np.uint64]
+HashElm = Callable[npt.NDArray[str], np.uint64]
 
 HashSet = List[HashElm]
-def hash_element(string, hash_gen: Callable[[str], np.uint64], salt: str) -> HashElm:
-    # pad the salt with zeros
-    return hash_gen(string + salt)
 
-def gen_hash_functions(num_hashes: int, salt_len: int = 20) -> HashSet:
+
+
+def hash_element(tokens: npt.NDArray[str], hash_gen_vec: Callable[npt.NDArray[str], np.uint64], salt: str) -> HashElm:
+    # pad the salt with zeros
+    return hash_gen_vec(np.char.add(tokens, salt))
+
+
+def gen_hash_functions(num_hashes: int, salt_len: int = 20, hash_element_fn = hash_element) -> HashSet:
+    """Generate a list of hash functions"""
     salt = np.random.randint(0, 10000000000, num_hashes)
     salt_strings = np.char.zfill(salt.astype(str), salt_len)[-salt_len:]
-    return [partial(farmhash.hash64, salt=salt) for salt in salt_strings]
-    
-
-
-# 1. Trying to recreate minhash from scratch
-def calcHash(token: np.int64, )
+    return [partial(hash_element_fn, hash_gen_vec=np.vectorize(farmhash.hash64), salt=salt) for salt in salt_strings]
 
 
 
-def genSalt(length: int) -> np.int64:
+
