8a1d803029c5d6b5cbe395049a1e6ae9aed5b3d9
liz <lizjunkbox@gmail.com>
2023-11-01 23:16:45 -0700

    renaming

--- min_hash.py
+++ min_hash.py
@@ -11,7 +11,7 @@ import numpy.typing as npt
 HashElm = Callable[[npt.NDArray[np.str_]], np.uint64]
 
 HashSet = List[HashElm]
-test_strings = [
+_test_strings = [
     "The quick brown fox jumps over the lazy dog. The dog was too lazy to react. The fox had a great time. In the distance, a bird chirped. The sun was setting, casting long shadows across the landscape. The fox looked around, its eyes gleaming in the fading light.",
     "The lazy dog decided to sleep all day. The quick brown fox was too busy jumping around to notice. The grass was green and lush, a perfect bed for a lazy dog. The dog yawned and stretched, then settled down for a nap. The fox continued to jump around, oblivious to the dog's slumber.",
     "The fox and the dog are good friends. They spend their days playing and jumping around. The forest was their playground, full of interesting smells and sights. They chased each other through the trees, their laughter echoing through the forest. At the end of the day, they would lie down side by side, content and tired.",
@@ -42,10 +42,10 @@ def gen_hash_functions(
 
 
 def compute_signature(
-    tokens: npt.NDArray[np.str_], hash_functions: HashSet
+    ngrams: npt.NDArray[np.str_], hash_functions: HashSet
 ) -> npt.NDArray[np.uint64]:
-    """Compute the signature for a set of tokens"""
-    return np.array([np.min(h(tokens)) for h in hash_functions])
+    """Compute the signature for a set of ngrams"""
+    return np.array([np.min(h(ngrams)) for h in hash_functions], dtype=np.uint64)
 
 
 def simple_shingles_generator(text: str, k: int = 3) -> npt.NDArray[np.str_]:
@@ -65,7 +65,7 @@ def jaccard_similarity(a: npt.NDArray[np.uint64], b: npt.NDArray[np.uint64]) -> 
     return np.intersect1d(a, b).size / np.union1d(a, b).size
 
 
-def _test_gen_hash_functions(test_strings=test_strings):
+def _test_gen_hash_functions(test_strings=_test_strings):
     """Test the hash function generator"""
     hash_functions = gen_hash_functions(20, 10)
     sigs = []
