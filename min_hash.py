1578dcb21fb4fd2b4992a60623871444d514f8b8
liz <lizjunkbox@gmail.com>
2023-11-02 00:18:39 -0700

    switched to openai token encodings and faster hashing algo

--- min_hash.py
+++ min_hash.py
@@ -1,12 +1,20 @@
 from functools import partial
 from typing import Callable, List
 
-import farmhash
 import numpy as np
 import numpy.typing as npt
+import tiktoken
+from xxhash import xxh32
 
+enc = tiktoken.get_encoding("cl100k_base")
 # from nptyping import NDArray, Structure, Shape, String
 
+
+def hash_numpy(token: np.uint64, seed: np.uint64) -> np.uint64:
+    return xxh32(bytes(token), int(seed)).intdigest()
+
+
+hash_numpy_vec = np.vectorize(hash_numpy)
 
 HashElm = Callable[[npt.NDArray[np.str_]], np.uint64]
 
@@ -21,31 +29,36 @@ _test_strings = [
 
 
 def hash_element(
-    tokens: npt.NDArray[np.str_],
-    hash_gen_vec: Callable[[npt.NDArray[np.str_]], np.uint64],
-    salt: str,
+    tokens: npt.NDArray[np.uint64],
+    hash_gen_vec: Callable[[npt.NDArray, int], np.uint64],
+    salt: np.uint64,
 ) -> HashElm:
     # pad the salt with zeros
-    return hash_gen_vec(np.char.add(tokens, salt))
+    return hash_gen_vec(tokens, int(salt))
 
 
-def gen_hash_functions(
-    num_hashes: int, salt_len: int = 20, hash_element_fn=hash_element
-) -> HashSet:
+# def gen_hash_functions(
+#     num_hashes: int, salt_len: int = 20, hash_element_fn=hash_element
+# ) -> HashSet:
+#     """Generate a list of hash functions"""
+#     salt = np.random.randint(0, 10000000, num_hashes)
+#     salt_strings = np.char.zfill(salt.astype(str), salt_len)[-salt_len:]
+#     return [
+#         partial(hash_element_fn, hash_gen_vec=hash_numpy_vec, salt=salt)
+#         for salt in salt_strings
+#     ]
+def gen_hash_functions(num_hashes: int, hash_element_fn=hash_element) -> HashSet:
     """Generate a list of hash functions"""
     salt = np.random.randint(0, 10000000, num_hashes)
-    salt_strings = np.char.zfill(salt.astype(str), salt_len)[-salt_len:]
-    return [
-        partial(hash_element_fn, hash_gen_vec=np.vectorize(farmhash.hash32), salt=salt)
-        for salt in salt_strings
-    ]
+    # salt_strings = np.char.zfill(salt.astype(str), salt_len)[-salt_len:]
+    return [partial(hash_element_fn, hash_gen_vec=hash_numpy_vec, salt=s) for s in salt]
 
 
 def compute_signature(
-    ngrams: npt.NDArray[np.str_], hash_functions: HashSet
+    tokens: npt.NDArray[np.uint64], hash_functions: HashSet
 ) -> npt.NDArray[np.uint64]:
     """Compute the signature for a set of ngrams"""
-    return np.array([np.min(h(ngrams)) for h in hash_functions], dtype=np.uint64)
+    return np.array([np.min(h(tokens)) for h in hash_functions], dtype=np.uint64)
 
 
 def simple_shingles_generator(text: str, k: int = 3) -> npt.NDArray[np.str_]:
@@ -71,8 +84,9 @@ def _test_gen_hash_functions(test_strings=_test_strings):
     sigs = []
     tokenlist = []
     for elm in test_strings:
-        tokens = simple_shingles_generator(elm)
+        tokens = np.array(enc.encode_ordinary(elm), dtype=np.uint64)
         tokenlist.append(tokens)
+        print(tokens)
         sigs.append(compute_signature(tokens, hash_functions))
 
     # Compare jaccard similarity between all pairs of documents
