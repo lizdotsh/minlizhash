b6f5309164844e9495f692f7ee55babe550f19c6
liz <lizjunkbox@gmail.com>
2023-11-07 14:23:41 -0800

    cleanup

--- minlizhash/jit.py
+++ minlizhash/jit.py
@@ -3,81 +3,32 @@ from typing import Callable
 
 import numpy as np
 import numpy.typing as npt
-from numba import jit, njit
+from numba import njit
 from numba.typed import List
 
-from .types import (
-    Document,
-    DocumentSignature,
-    DocumentSigner,
-    PermutationSeeds,
-    TokenArray,
-)
-
-FNV_32_PRIME = 0x01000193
-FNV_64_PRIME = 0x100000001B3
-
-FNV0_32_INIT = 0
-FNV0_64_INIT = 0
-FNV1_32_INIT = 0x811C9DC5
-FNV1_32A_INIT = FNV1_32_INIT
-FNV1_64_INIT = 0xCBF29CE484222325
-FNV1_64A_INIT = FNV1_64_INIT
+from .types import DocumentSigner, PermutationSeeds, TokenArray
 
 
 @njit
-def int32_to_bytes(x):
+def int32_to_bytes(x: np.int32) -> tuple:
     return (x >> 24 & 0xFF, x >> 16 & 0xFF, x >> 8 & 0xFF, x & 0xFF)
 
 
 @njit
-def int64_to_bytes(x):
-    return (
-        x >> 56 & 0xFF,
-        x >> 48 & 0xFF,
-        x >> 40 & 0xFF,
-        x >> 32 & 0xFF,
-        x >> 24 & 0xFF,
-        x >> 16 & 0xFF,
-        x >> 8 & 0xFF,
-        x & 0xFF,
-    )
-
-
-@njit
-def fnv1a32(data: np.int32, seed: np.int32):
+def fnv1a32(num: np.int32, seed: np.int32) -> np.uint64:
     """
-    Returns the 32 bit FNV-1a hash value for the given data.
+    Returns the 32 bit FNV-1a hash value for the given number.
     """
-    # assert isinstance(data, bytes)
 
     hval = 0x811C9DC5 ^ seed
-    for byte in int32_to_bytes(data):
+    for byte in int32_to_bytes(num):
         hval = hval ^ byte
         hval = (hval * 0x01000193) % 2**32
     return np.uint64(hval)
 
 
 @njit
-def fnv1a64(data: np.int32, seed: np.int32):
-    """
-    Returns the 64 bit FNV-1a hash value for the given data.
-    """
-    # assert isinstance(data, bytes)
-
-    hval = 0xCBF29CE484222325 ^ seed
-    for byte in int64_to_bytes(data):
-        hval = hval ^ byte
-        hval = (hval * 0x100000001B3) % 2**64
-    return hval
-
-
-@njit
-def minhash_jit(
-    tokens: npt.NDArray[np.int32],
-    hash_seed: np.int32,  # ,
-    # hash_fn: Callable[[bytes, int], int] = fnv1a,
-) -> np.uint64:
+def minhash_jit(tokens: npt.NDArray[np.int32], hash_seed: np.int32) -> np.uint64:
     empty: npt.NDArray[np.uint64] = np.empty((tokens.shape[0],), dtype=np.uint64)
     for i in range(tokens.shape[0]):
         empty[i] = fnv1a32(tokens[i], hash_seed)
